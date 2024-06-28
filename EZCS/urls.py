d4550d6e464a9e2e6c8728aedae0bbeb9dfe1603
Cho_c <ghktmsgkrh@naver.com>
2024-06-28 23:03:48 +0900

    UPDATE WARNINGS: URL namespace 'main' isn't unique.

--- EZCS/urls.py
+++ EZCS/urls.py
@@ -20,7 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
-    path("main/", include("main.urls")),
     path("accounts/", include("accounts.urls")),
     path("management/", include("management.urls")),
     path("education/", include("education.urls")),

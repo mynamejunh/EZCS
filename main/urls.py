153803e285b065b28fd9aa45c0c30a304fd41cb6
Heather <betterthanbe4.2000@gmail.com>
2024-06-28 15:51:49 +0900

    full from dev

--- main/urls.py
+++ main/urls.py
@@ -5,8 +5,4 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.user_dashboard, name='user_dashboard'),
-<<<<<<< HEAD
-    path('manager/', views.manager_dashboard, name='manager_dashboard'),
-=======
->>>>>>> 783d685edba33a5b57c4cf9911d75b11976edfac
 ]

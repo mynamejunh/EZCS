71a8c8e06628ed556d0f10000915f60b81ec3fe9
Cho_c <ghktmsgkrh@naver.com>
2024-06-30 17:33:59 +0900

    UPDATE add user name

--- main/views.py
+++ main/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def user_dashboard(request):
-    return render(request, 'main/index.html')
+    user = request.session.get('user', None)
+
+    data = {
+        'user': user
+    }
+    
+    return render(request, 'main/index.html', data)

03ce12c2ac4313c8ca118aefd4a2bed370def54e
Cho_c <ghktmsgkrh@naver.com>
2024-06-30 17:32:24 +0900

    ADD login session

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     #path('signup/', views.signup, name='signup'),
     #path('login/', views.login_view, name='login'),
     path('', views.login, name='login'),
-    #path('logout/', views.logout_view, name='logout'),
+    path('logout/', views.logout, name='logout'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import User
 
 def login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        try:
-            user = User.objects.get(username=username)
-            print(user.password)
-            if password == user.password:
-                result = 'success'
-            else:
-                result = 'Incorrect password'
-        except User.DoesNotExist:
-            result = 'User does not exist'
+    if request.method == 'GET':
+        return render(request, 'accounts/login.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        
+        if not (username and password):
+            result = '데이터가 안들어옴'
+        else:
+            try:
+                user = User.objects.get(username=username)
+                if password == user.password:
+                    request.session['user'] = username
+                    result = 'success'
+                else:
+                    result = 'Incorrect password'
+            except User.DoesNotExist:
+                result = 'User does not exist'
+    else:
+        result = 'request.method != POST'
 
-        return JsonResponse({'result': result})
-    return render(request, 'accounts/login.html')
+    return JsonResponse({'result': result})
+
+def logout(request):
+    request.session.pop('user')
+    return redirect('/')

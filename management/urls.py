8056b61bb6cc6fec05aff6872dc7b8e473850a5b
myeoungseon <141654355+myeoungseon@users.noreply.github.com>
2024-07-01 17:30:40 +0900

    management 코드 (#40)
    * admin
    
    * management
    
    * management
    
    * management bms
    
    * management bms

--- management/urls.py
+++ management/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('', views.manager_dashboard, name='manager_dashboard'),
     path('management_detail/<int:id>/', views.manager_detail, name='management_detail'),
     path('management_detail/<int:id>/edit', views.manager_edit, name='management_edit'),
+    path('allow/', views.allow, name='allow'),
+    path('approve/<int:id>/', views.approve_user, name='approve_user'),
+    # path('reject/<int:id>/', views.reject_user, name='reject_user'),
+    path('quitter/', views.quitter, name='quitter'),
 ]

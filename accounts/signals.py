43b0007fe80880fcd4487c0c7fd66dada1ad4a04
Cho_c <ghktmsgkrh@naver.com>
2024-07-01 18:01:37 +0900

    ADD signals

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,9 +1,13 @@
 import logging
-from django.contrib.auth.signals import user_logged_in, user_logged_out
 from django.dispatch import receiver
+from django.db.models.signals import post_save
+from .models import UserSession
 
 
-@receiver(user_logged_in)
+@receiver(post_save, sender = UserSession)
 def sig_user_logged_in(sender, user, request, **kwargs):
+    print('='*100)
+    print("haha")
+    print('='*100)
     logger = logging.getLogger(__name__)
     logger.debug("user logged in: %s at %s" % (user, request.META['REMOTE_ADDR']))

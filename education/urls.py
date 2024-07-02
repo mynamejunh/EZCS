9574c89a22f2daf1994c919c1d8635d94b9e654f
Heather <betterthanbe4.2000@gmail.com>
2024-07-02 17:27:19 +0900

    update chat.html for testing

--- education/urls.py
+++ education/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('quiz/', views.quiz, name='quiz'),
     path('details/', views.details, name='details'),  
     # 일단 어떻게 연결해야할지 모르겠어서 일단 연결해뇠는데, history에서 확인 누르면 해당 기록 열려야함
+
+    # chat 어떻게 동작되는지 보는 용도의 urls
+    path('chat/', views.chat_view_test, name='chat_view_test'), 
 ]

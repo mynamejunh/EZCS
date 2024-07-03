7e5415620614acae8fbb63babe893d289955fa74
Cho_c <ghktmsgkrh@naver.com>
2024-07-03 14:03:11 +0900

    UPDAT use django.contrib.auth

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,6 @@ from django.http import JsonResponse
 from .models import User
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 import re
-from django.http import HttpResponse
 import logging
 from django.contrib.auth import password_validation
 from django.core.exceptions import ValidationError
@@ -12,54 +11,57 @@ logger = logging.getLogger(__name__)
 
 
 def login(request):
+    """
+        request.method == 'GET': 접속
+        request.method == 'POST': 로그인/관리자 로그인
+        request.method == *: 잘못된 접근
+    """
+
     if request.method == 'GET':
-        # 추가
         context = {}
         if 'remember_me' in request.COOKIES:
             context['username'] = request.COOKIES['remember_me']
             context['remember_me'] = True  
         return render(request, 'accounts/login.html', context)
-
     elif request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
         remember_me = request.POST.get('remember_me', None)
 
-        if not (username and password):
-            result = '아이디와 비밀번호를 입력하세요'
-        else:
-            try:
-                user = User.objects.get(username=username)
-                if not user.is_active:
-                    result = '관리자의 승인이 필요합니다.'
-                elif password == user.password:
-                    request.session['user'] = username
-                    if user.is_superuser == True:
-                        result = 'manager'
-                    else:
-                        result = 'user'
-                    # 수정
-                    response = JsonResponse({'result' : result})
-                    if remember_me == 'on':
-                        response.set_cookie('remember_me', username, max_age=2592000)
-                    else:
-                        response.delete_cookie('remember_me')
-                    return response
+        user = authenticate(request, username=username, password=password)
+        logger.log(1, user)
 
+        if user is not None:
+            if not user.is_active:
+                result = '관리자의 승인이 필요합니다.'
+            else:
+                auth_login(request, user)
+                request.session['user'] = username
+                if user.is_superuser == True:
+                    result = 'manager'
                 else:
-                    result = '비밀번호가 올바르지 않습니다'
-            except User.DoesNotExist:
-                result = '해당 사용자가 존재하지 않습니다'
+                    result = 'user'
+                
+                response = JsonResponse({'result' : result})
+                if remember_me == 'on':
+                    response.set_cookie('remember_me', username, max_age=2592000)
+                else:
+                    response.delete_cookie('remember_me')
+                return response
+        else:
+            result = '해당 사용자가 존재하지 않습니다'
+    else:
+        result = '잘못된 접근입니다.'
     return JsonResponse({'result': result})
 
+
 def logout(request):
+    """
+        로그아웃
+    """
     auth_logout(request)
-    #request.session.pop('user')
     return redirect('/')
 
-
-def adminLogin(request):
-    return render(request, 'accounts/adminlogin.html')
 
 def searchPW(request):
     return render(request, 'accounts/searchpw.html')
@@ -68,6 +70,7 @@ def searchPW(request):
 def validate_username(username):
     if re.search(r'[ㄱ-ㅎㅏ-ㅣ가-힣]', username):
         raise ValidationError('아이디는 한글을 포함할 수 없습니다.')
+
 
 def validate_name(name):
     if not re.match(r'^[가-힣]+$', name):
@@ -132,6 +135,7 @@ def signup(request):
         return JsonResponse({'success': False, 'errors': errors})
 
     return render(request, 'accounts/signup.html')
+
 
 def check_username(request):
     username = request.GET.get('username')

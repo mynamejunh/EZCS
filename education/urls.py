541b936405367eccb4b756628436d524cddae708
Heather <119404818+Heather-2000@users.noreply.github.com>
2024-07-04 10:06:34 +0900

    Update templates (#76)
    * Remake Repo
    
    * Create GitCommand.md
    
    * Update GitCommand.md
    
    * Update README.md
    
    * Update From wonjunRD
    
    * Move dev from hs (#19)
    
    * Remake Repo
    
    * Create GitCommand.md
    
    * Update GitCommand.md
    
    * Update README.md
    
    * Update From wonjunRD
    
    * Update settings.py
    
    * update layout for both user
    
    * Update FE (#24)
    
    * Remake Repo
    
    * Create GitCommand.md
    
    * Update GitCommand.md
    
    * Update README.md
    
    * Update From wonjunRD
    
    * Update counseling/models.py (#18)
    
    * Implements counseling/models.py
    
    * Update counseling/models.py
    
    ---------
    
    Co-authored-by: ChoWonJun999 <62541405+ChoWonJun999@users.noreply.github.com>
    
    * makemigrations
    
    * Delete accounts/migrations directory
    
    * Delete accounts/__pycache__ directory
    
    * Delete counseling/__pycache__ directory
    
    * Delete counseling/migrations directory
    
    * Delete education/migrations directory
    
    * Delete education/__pycache__ directory
    
    * Delete main/migrations directory
    
    * Delete main/__pycache__ directory
    
    * Delete management/__pycache__ directory
    
    * Delete management/migrations directory
    
    * Delete EZCS/__pycache__ directory
    
    * Implement initial database structure and models (#20)
    
    * Implements counseling/models.py
    
    * Update counseling/models.py
    
    * Implements education/models.py
    
    * Add gitignore
    
    ---------
    
    Co-authored-by: ChoWonJun999 <62541405+ChoWonJun999@users.noreply.github.com>
    
    * Implements Admin Page & fix bug (#21)
    
    * Delete accounts/migrations directory
    
    * Delete counseling/migrations directory
    
    * Delete education/migrations directory
    
    * update layout for both user
    
    * UPDATE .gitignore
    
    ---------
    
    Co-authored-by: Jin <82833598+JinSeongjin@users.noreply.github.com>
    Co-authored-by: ChoWonJun999 <62541405+ChoWonJun999@users.noreply.github.com>
    Co-authored-by: cwj <dod875@hanmail.net>
    
    * Delete accounts/__pycache__ directory
    
    * Delete accounts/migrations/__pycache__ directory
    
    * Delete counseling/__pycache__ directory
    
    * Delete counseling/migrations/__pycache__ directory
    
    * Delete education/__pycache__ directory
    
    * Delete education/migrations/__pycache__ directory
    
    * Delete main/__pycache__ directory
    
    * Delete main/migrations/__pycache__ directory
    
    * Delete management/__pycache__ directory
    
    * Delete management/migrations/__pycache__ directory
    
    * full from dev
    
    * update base.html
    
    * add userSetting page
    
    * update urls
    
    * 'fixed'
    
    * Update templates
    
    * update layouts
    
    * delete pycache
    
    * merged chatbot to edu
    
    * fixed accounts/urls.py
    
    * update education and base.html
    
    * Remove migrations folders
    
    * update chat.html for testing
    
    * update chat.html for testing
    
    * update edu/tem and views
    
    * update base_test.html
    
    * update base_test.html
    
    * Move bootstrap files
    
    * update new templates
    
    * update counseling.html
    
    * update counseling
    
    * fixed customer-form error
    
    * update counseling html/css
    
    * fixed css error of education index.html
    
    * update styles.css
    
    * complete chatbot css anyway
    
    ---------
    
    Co-authored-by: Jin <82833598+JinSeongjin@users.noreply.github.com>
    Co-authored-by: ChoWonJun999 <62541405+ChoWonJun999@users.noreply.github.com>
    Co-authored-by: cwj <dod875@hanmail.net>

--- education/urls.py
+++ education/urls.py
@@ -6,6 +6,7 @@ app_name = 'education'
 urlpatterns = [
     path('', views.chat_view, name='chat_view'),
     path('edu_history/', views.edu_history, name='edu_history'),
+    # path('edu_detalis/<int:id>/', views.edu_detail, name='edu_detali'),
     path('quiz/', views.quiz, name='quiz'),
     path('details/', views.details, name='details'),  
     # 일단 어떻게 연결해야할지 모르겠어서 일단 연결해뇠는데, history에서 확인 누르면 해당 기록 열려야함

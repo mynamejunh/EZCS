63d143bac5a3f52c8e9e497bdc9b5b8762cebc20
Heather <betterthanbe4.2000@gmail.com>
2024-07-04 17:11:19 +0900

    update quiz templates

---
+++ education/templatetags/quiz_tags.py
@@ -0,0 +1,9 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def get_item(dictionary, key):
+    if dictionary is None or not isinstance(dictionary, dict):
+        return None
+    return dictionary.get(key)

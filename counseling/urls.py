7422c4826281ef9d0ae720d9e90cbfa5883e5b45
Cho_c <ghktmsgkrh@naver.com>
2024-07-11 16:38:10 +0900

    delete stt

--- counseling/urls.py
+++ counseling/urls.py
@@ -6,12 +6,7 @@ app_name = "counseling"
 urlpatterns = [
     path("", views.list, name="list"),
     path("history/", views.history, name="history"),
-    # path("test", views.test, name="test"),
-    path("stt/", views.stt, name="stt"),
-    path("stt_chat/", views.stt_chat, name="stt_chat"),
     path("save_customer_info/", views.save_customer_info, name="save_customer_info"),
     path("save_counseling_log/", views.save_counseling_log, name="save_counseling_log"),
     path("save_consultation/", views.save_consultation, name="save_consultation"),
-
-    # path('customer_detail/', views.customer_detail, name='customer_detail')
 ]
